57d3e386d2559c5be153d7da830c9c0b29a2d084
David Edysonn Belfort Neves <34004075+davidedysonn@users.noreply.github.com>
2026-03-31 10:14:02 -0300

    feat: adicao de services

---
+++ src/main/java/com/davidbelfort/order_management_api/services/CustomerService.java
@@ -0,0 +1,12 @@
+package com.davidbelfort.order_management_api.services;
+
+
+import com.davidbelfort.order_management_api.entities.Customer;
+
+public interface CustomerService {
+    Customer create (Customer custumer);
+    Customer read (Long id);
+    Customer readName (String name);
+    Customer update (Long id);
+    Void delete (Long id);
+}
---
+++ src/main/java/com/davidbelfort/order_management_api/services/OrderService.java
@@ -0,0 +1,4 @@
+package com.davidbelfort.order_management_api.services;
+
+public interface OrderService {
+}
