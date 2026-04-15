883b8523cb74bb31bfd56b20a3d6a7cd5c7eecc0
David Belfort <david.belfort@ssp.ma.gov.br>
2026-04-15 14:56:54 -0300

    fix: tipo de metodo.

--- src/main/java/com/davidbelfort/order_management_api/services/OrderService.java
+++ src/main/java/com/davidbelfort/order_management_api/services/OrderService.java
@@ -5,7 +5,6 @@ import com.davidbelfort.order_management_api.entities.Order;
 public interface OrderService {
     Order create (Order order);
     Order read (Long id);
-    Order readName (String name);
-    Order update (Long id);
-    Void delete (Long id);
+    Order update (Long id, Order order);
+    void delete (Long id);
 }
