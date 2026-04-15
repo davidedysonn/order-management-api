c7ba93a30292d0edae69d0e57a0440cb1571f957
David Belfort <david.belfort@ssp.ma.gov.br>
2026-04-15 14:57:54 -0300

    feat: adicionado metodo de busca por nome do cliente.

--- src/main/java/com/davidbelfort/order_management_api/repositories/CustomerRepository.java
+++ src/main/java/com/davidbelfort/order_management_api/repositories/CustomerRepository.java
@@ -3,5 +3,8 @@ package com.davidbelfort.order_management_api.repositories;
 import com.davidbelfort.order_management_api.entities.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    Optional<Customer> findByName (String name);
 }
